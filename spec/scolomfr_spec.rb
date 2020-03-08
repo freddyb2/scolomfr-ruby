a4a787db619660e21ec7f423162009d2c2bccbe1
Fred <frederic.boisguerin@gmail.com>
2020-03-08 10:46:31 +0100

    Indentation

--- spec/scolomfr_spec.rb
+++ spec/scolomfr_spec.rb
@@ -3,11 +3,13 @@ require 'spec_helper'
 describe Scolomfr do
   EXAMPLE_NOTICE = 'assets/ExempleManuelReferenceScoLOMFRv3-2.xml'
   EXAMPLE_NOTICE_WITH_ERRORS = 'assets/ExempleManuelReferenceScoLOMFRv3-2_with_error.xml'
+
   context 'reference manual notice' do
     it 'returns no ERROR' do
       expect(Scolomfr.is_notice_valid? EXAMPLE_NOTICE).to eq true
     end
-    end
+  end
+
   context 'reference manual notice with errors' do
     it 'returns false' do
       expect(Scolomfr.is_notice_valid? EXAMPLE_NOTICE_WITH_ERRORS).to eq false
